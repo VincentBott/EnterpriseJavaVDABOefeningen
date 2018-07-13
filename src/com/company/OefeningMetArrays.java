fb236deffadb51c6292b6463666ab6a1fcd8415d
VincentBott <vincentbott@gmail.com>
2018-07-13 13:27:47 +0200

    Oefening met Arrays.

---
+++ src/com/company/OefeningMetArrays.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class OefeningMetArrays {
+    public static void main(String[] args) {
+        
+        String[] namen = {"Joske", "Louiske", "Franske"};
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
