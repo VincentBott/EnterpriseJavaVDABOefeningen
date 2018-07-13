bf95f83e4add44f7e45d33fbd88a4a9f7cbf74b1
VincentBott <vincentbott@gmail.com>
2018-07-13 13:37:28 +0200

    Oefening met Arrays.

---
+++ src/com/company/ArraysDeelbaarDoorTwee.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ArraysDeelbaarDoorTwee {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] getallen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < getallen.length; i++) {
+            if (getallen[i] % 2 == 0)
+                System.out.println(getallen[i]);
+        }
+
+
+        for(int getal: getallen) {
+            if (getal % 2 == 0)
+                System.out.println(getal);
+
+        }
+    }
+}
--- src/com/company/OefeningMetArrays.java
+++ src/com/company/OefeningMetArrays.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class OefeningMetArrays {
     public static void main(String[] args) {
         
-        String[] namen = {"Joske", "Louiske", "Franske"};
+        String[] namen = {"Joske", "Louiske", "Franske", "Jefke"};
 
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 1; i <= namen.length ; i++) {
+            System.out.println("Naam " + i + ": " + namen[i-1]);
+        }
+
+        System.out.println();
+
+        for (String naam: namen) {
+            System.out.println(naam);
         }
     }
 }
