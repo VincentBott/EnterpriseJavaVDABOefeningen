dd6231133e720fc7711ead0a8fb355a08acffc26
VincentBott <vincentbott@gmail.com>
2018-07-13 12:24:54 +0200

    Nieuw bestand. Palindroom. Aangepast.

--- src/com/company/Palindroom.java
+++ src/com/company/Palindroom.java
@@ -5,6 +5,42 @@ import java.util.Scanner;
 public class Palindroom {
     public static void main(String[] args) {
 
+        /*
+        Geef een woord:lepel
+        lepel is een palindroom.
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        String ingegevenWoord;
+
+        System.out.println("Geef een woord: ");
+
+        ingegevenWoord = scanner.nextLine();
+
+    //    char[] woordArray = ingegevenWoord.toCharArray();
+
+        String omgekeerdWoord = "";
+        char[] omgekeerdWoordArray = ingegevenWoord.toCharArray();
+
+        int lengteWoord = ingegevenWoord.length();
+
+        for (int i = 0; i < lengteWoord; i++) {
+            omgekeerdWoordArray[i] = ingegevenWoord.charAt(i);
+        }
+
+        omgekeerdWoord = new String (omgekeerdWoordArray);
+
+        if (ingegevenWoord.equalsIgnoreCase(omgekeerdWoord))
+            System.out.println(ingegevenWoord + " is een palindroom.");
+        else
+         System.out.println(ingegevenWoord + " is geen palindroom");
+
+
+        System.out.println(ingegevenWoord);
+        System.out.println(omgekeerdWoord);
+
+
 
     }
 }
