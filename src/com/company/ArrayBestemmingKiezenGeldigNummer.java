5f39d17498f1be6c550513fea224c0ad2a8e38d3
VincentBott <vincentbott@gmail.com>
2018-07-13 15:49:57 +0200

    Array met bestemmingen te kiezen en met een While-loop voor een geldige keuze.

---
+++ src/com/company/ArrayBestemmingKiezenGeldigNummer.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ArrayBestemmingKiezenGeldigNummer {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] bestemmingen = {"Provence", "Zwitserland", "Bretagne"};
+
+        System.out.print("Geef je naam: ");
+
+        String naam = scanner.nextLine();
+
+        int keuze;
+
+        do {
+            System.out.println("Kies een bestemming: ");
+
+            for (int i = 0; i < bestemmingen.length; i++)
+                System.out.printf("\t%d. %s\n", (i + 1), bestemmingen[i]);
+
+            System.out.print("Welke bestemming kies je (1-3) ? ");
+
+            keuze = Integer.parseInt(scanner.nextLine());
+
+            if (keuze < 1 || keuze > 3)
+                System.out.println("Kies een geldig nummer.");
+
+        } while (keuze < 1 || keuze > 3);
+
+        System.out.printf("Dag %s, goede reis naar %s." , naam, bestemmingen[keuze-1]);
+
+    }
+}
