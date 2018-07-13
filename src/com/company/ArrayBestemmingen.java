c2d3b43063dcd29c7ae5614c7f22fbe7ceb55f38
VincentBott <vincentbott@gmail.com>
2018-07-13 15:05:06 +0200

    Array met bestemmingen.

---
+++ src/com/company/ArrayBestemmingen.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ArrayBestemmingen {
+    public static void main(String[] args) {
+
+        String[] bestemmingen = {"Provence", "Zwitserland", "Bretagne"};
+
+
+        System.out.println("Kies een bestemming: ");
+
+        for (int i = 0; i < bestemmingen.length ; i++) {
+            System.out.printf("\t%d. %s\n", (i+1), bestemmingen[i]);
+
+        }
+    }
+}
