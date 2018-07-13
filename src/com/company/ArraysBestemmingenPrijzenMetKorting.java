4dc96335ab145e44d2e1b9e099354e1dbfb1791f
VincentBott <vincentbott@gmail.com>
2018-07-13 23:08:56 +0200

    Met korting.

--- src/com/company/ArraysBestemmingenPrijzenMetKorting.java
+++ src/com/company/ArraysBestemmingenPrijzenMetKorting.java
@@ -18,12 +18,13 @@ public class ArraysBestemmingenPrijzenMetKorting {
 
         int keuze;
 
+        double teBetalen;
 
         do {
             System.out.println("Kies een bestemming: ");
 
             for (int i = 0; i < bestemmingen.length; i++) {
-                System.out.printf("\t%d. %s (€%.2f)\n", (i + 1), bestemmingen[i], prijzen[i]);
+                System.out.printf("\t%d. %s (€ %.2f)\n", (i + 1), bestemmingen[i], prijzen[i]);
 
             }
 
@@ -37,8 +38,12 @@ public class ArraysBestemmingenPrijzenMetKorting {
 
         } while (keuze < 1 || keuze > 3);
 
-        if
-        System.out.printf("Dag %s, goede reis naar %s. Dit kost je € %.2f" , naam, bestemmingen[keuze-1], prijzen[keuze-1]);
+        if (prijzen[keuze-1] > 300)
+            teBetalen = prijzen[keuze-1] * 0.90;  // 10% korting
+        else
+            teBetalen = prijzen[keuze-1];
+
+        System.out.printf("Dag %s, goede reis naar %s. Dit kost je € %.2f." , naam, bestemmingen[keuze-1], teBetalen);
 
     }
 }
