0c9df42a1fa13258099a7ad262d0c6981ec9fa11
VincentBott <vincentbott@gmail.com>
2018-08-07 15:02:15 +0200

    Extra oefening.

---
+++ src/com/company/VraagNamenEnPunten.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.time.LocalDate;
+
+import java.util.Scanner;
+
+
+public class VraagNamenEnPunten {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+    }
+}
